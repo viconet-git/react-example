dc60ca6d8f6f2401df63dfb961bc86b50944a0db
vico <victorqcv@gmail.com>
2020-10-31 10:22:40 -0400

    primer commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          React aplication - This is my aplication for Vico Net
         </p>
         <a
           className="App-link"
